6e7f65515ecc25e39cad5aee42774e59a7a7d668
shanfy <815481278@qq.com>
2022-10-04 17:20:52 +0800

    jdk8调试项目

---
+++ learn_jdk/src/main/java/util/ArrayListTest.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: shanfy
+ * @Date: 2022/10/4 17:03
+ */
+public class ArrayListTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println(list);
+    }
+}
---
+++ learn_jdk/src/main/java/util/LinkedListTest.java
@@ -0,0 +1,8 @@
+package util;
+
+/**
+ * @Author: shanfy
+ * @Date: 2022/10/4 17:19
+ */
+public class LinkedListTest {
+}
